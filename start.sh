a8ecd54c40967054e7fdace48e0587da1ee56cfe
byu <byu@tibco-support.com>
2016-05-24 13:16:12 +0800

    Fixed the start.sh script.

--- start.sh
+++ start.sh
@@ -4,15 +4,18 @@
 # Author : Burt
 # Script follows here:
 
+echo "Modifying the file Competence..."
+chmod -R 777 storage/
+chmod -R 777 bootstrap/cache/
+echo "Modifying the file Competence is done"
+
 file="./.env"
 
 if [ ! -f "$file" ]; then
-    echo "Modified the file Competence..."
-    chmod -R 777 storage/
-    chmod -R 777 bootstrap/cache/
+    echo 'Create the ".env" configuration file...'
     cp .env.example .env
-    php artisan key:gennerate
+    php artisan key:generate
     echo 'Create the config variable file ".env", you can modified that file to modified the project configuration.'
 else
-    echo 'You had installed this project.'
+    echo 'Congratulations! You had installed this project, you can start to work on lsApi, If you still can not running it, please check "README.md" file again.'
 fi
