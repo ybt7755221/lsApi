618db84ab9d8561df1f07d1d559e8591dd447d68
byu <byu@tibco-support.com>
2016-06-21 13:18:29 +0800

    format all view pages.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,81 +1,81 @@
 @extends('layouts.auth')
-
 @section('content')
-<div class="container">
+  <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{trans('system.title')}} -- {{ trans('system.login') }}</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+      <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+          <div class="panel-heading">{{trans('system.title')}} -- {{ trans('system.login') }}</div>
+          <div class="panel-body">
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+              {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">{{ trans('auth.email') }}</label>
+              <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label class="col-md-4 control-label">{{ trans('auth.email') }}</label>
 
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                <div class="col-md-6">
+                  <input type="email" class="form-control" name="email" value="{{ old('email') }}">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
+                  @if ($errors->has('email'))
+                    <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+                  @endif
+                </div>
+              </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">{{ trans('password') }}</label>
+              <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label class="col-md-4 control-label">{{ trans('password') }}</label>
 
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
+                <div class="col-md-6">
+                  <input type="password" class="form-control" name="password">
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
+                  @if ($errors->has('password'))
+                    <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember">{{ trans('auth.remember') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Login
-                                </button>
-                                <a class="btn btn-link" id="forget_password" data-toggle="modal" data-target="#myModal">{{ trans('auth.forgotPassword') }}</a>
-                            </div>
-                        </div>
-                    </form>
+                  @endif
                 </div>
-            </div>
+              </div>
+
+              <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox" name="remember">{{ trans('auth.remember') }}
+                    </label>
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                  <button type="submit" class="btn btn-primary">
+                    <i class="fa fa-btn fa-sign-in"></i>Login
+                  </button>
+                  <a class="btn btn-link" id="forget_password" data-toggle="modal"
+                     data-target="#myModal">{{ trans('auth.forgotPassword') }}</a>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-<div id="myModal" class="modal fade" role="dialog">
+  </div>
+  <div id="myModal" class="modal fade" role="dialog">
     <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h4 class="modal-title">{{trans('auth.forgotPassword')}}</h4>
-            </div>
-            <div class="modal-body">
-                <p>{{trans('auth.connectAdmin', ['email' => 'ybt7755221@sohu.com'])}}</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-        </div><!-- /.modal-content -->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+          <h4 class="modal-title">{{trans('auth.forgotPassword')}}</h4>
+        </div>
+        <div class="modal-body">
+          <p>{{trans('auth.connectAdmin', ['email' => 'ybt7755221@sohu.com'])}}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+  </div><!-- /.modal -->
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.auth')
-
 @section('content')
-<div class="container">
+  <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ trans('system.register') }}</div>
-                <div class="panel-body">
-                   <p>{{trans('errors.LS40301_NAME')}}: <br/ >{{trans('errors.LS40301_INFO')}}</p>
-                   <p><a href="{{url('/')}}">Return the login page</a></p>
-                </div>
-            </div>
+      <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+          <div class="panel-heading">{{ trans('system.register') }}</div>
+          <div class="panel-body">
+            <p>{{trans('errors.LS40301_NAME')}}: <br/ >{{trans('errors.LS40301_INFO')}}</p>
+            <p><a href="{{url('/')}}">Return the login page</a></p>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
