f52bf15291d6bb342dde80bf1a48f9e088c46685
byu <byu@tibco-support.com>
2016-07-19 16:50:59 +0800

    Finished field page.

--- app/Http/Controllers/FieldsController.php
+++ app/Http/Controllers/FieldsController.php
@@ -13,6 +13,101 @@ use Illuminate\Support\Facades\Session;
 class FieldsController extends Controller
 {
     /**
+     * Restful Api Function - Show the field for link.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(){
+        if( $this->userDisable($this->user_for_api->status, 'create') ){
+            $fieldsObj = Fields::all(['id', 'label', 'key', 'params', 'publish', 'field_type']);
+            if($fieldsObj){
+                return $this->successRes($fieldsObj);
+            }
+            return $this->errorRes(trans('errors.LS40401_UNKNOWN'));
+        }else{
+            return $this->errorRes(trans('validation.user.disabled_power',['op' => 'Create']));
+        }
+    }
+
+    /**
+     * Restful Api Function - Create a data.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request){
+        if( $this->userDisable($this->user_for_api->status, 'create') ){
+            $record = Fields::create([
+                    'label' => $request['label'],
+                    'key' => $request['key'],
+                    'params' => $request['params'],
+                    'publish' => $request['publish'],
+                    'field_type' => strtoupper($request['field_type']),
+            ]);
+            if($record){
+                return $this->successRes($record);
+            }else{
+                return $this->errorRes(trans('errors.LS40401_UNKNOWN'));
+            }
+        }
+        return $this->errorRes(trans('validation.user.disabled_power',['op' => 'create']));
+    }
+
+    /**
+     * Restful Api Function - Update a data.
+     *
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id){
+        if( $this->userDisable($this->user_for_api->status, 'update') ){
+            $create_data = [];
+            if(isset($request['label']) && !empty($request['label'])){
+                $create_data['label'] = $request['label'];
+            }
+            if(isset($request['key']) && !empty($request['key'])){
+                $create_data['key'] = $request['key'];
+            }
+            if(isset($request['params']) && !empty($request['params'])){
+                $create_data['params'] = $request['params'];
+            }
+            if(isset($request['publish']) && !empty($request['publish'])){
+                $create_data['publish'] = $request['publish'];
+            }
+            if(isset($request['field_type']) && !empty($request['field_type'])){
+                $create_data['field_type'] = $request['field_type'];
+            }
+            $result = Fields::find((int) $id)->update($create_data);
+            if ( $result ) {
+                $field = Fields::find((int) $id);
+                return $this->successRes($field);
+            }else{
+                return $this->errorRes(trans('errors.LS40401_UNKNOWN'));
+            }
+        }
+        return $this->errorRes(trans('validation.user.disabled_power',['op' => 'update']));
+    }
+
+    /**
+     * Restful Api Function - Removed a data.
+     *
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Request $request, $id){
+        if( $this->userDisable($this->user_for_api->status, 'delete') ){
+            $record = Fields::destroy($id);
+            if($record){
+                return $this->successRes(trans('validation.user.successful'));
+            }else{
+                return $this->errorRes(trans('errors.LS40401_UNKNOWN'));
+            }
+        }
+        return $this->errorRes(trans('validation.user.disabled_power',['op' => 'update']));
+    }
+    /**
      * create a fields.
      *
      * @param StoreFieldsPostRequest $request
